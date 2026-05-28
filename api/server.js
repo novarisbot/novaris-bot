3cfea0c062fe44cde95d350d8187c641c2cefcc9
novarisbot <kerembeyy3500@gmail.com>
2026-05-28 23:20:03 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -14,17 +14,55 @@ app.use(cors());
 
 app.use(express.json());
 
-/* announcement */
+const FILE =
+"./announcement.json";
+
+/* json oluştur */
+
+if(!fs.existsSync(FILE)){
+
+fs.writeFileSync(
+FILE,
+"[]"
+);
+
+}
+
+/* tüm duyurular */
+
+app.get(
+"/api/announcement",
+(req,res)=>{
+
+const data =
+JSON.parse(
+fs.readFileSync(FILE)
+);
+
+res.json(data);
+
+});
+
+/* duyuru ekle */
 
 app.post(
 "/api/announcement",
 (req,res)=>{
 
 const data =
-req.body;
+JSON.parse(
+fs.readFileSync(FILE)
+);
+
+const newAnnouncement = {
+id:Date.now(),
+...req.body
+};
+
+data.push(newAnnouncement);
 
 fs.writeFileSync(
-"./announcement.json",
+FILE,
 JSON.stringify(
 data,
 null,
@@ -38,20 +76,82 @@ success:true
 
 });
 
-/* get announcement */
+/* duyuru sil */
 
-app.get(
-"/api/announcement",
+app.delete(
+"/api/announcement/:id",
 (req,res)=>{
 
-const data =
+const id =
+Number(req.params.id);
+
+let data =
 JSON.parse(
-fs.readFileSync(
-"./announcement.json"
+fs.readFileSync(FILE)
+);
+
+data =
+data.filter(
+x => x.id !== id
+);
+
+fs.writeFileSync(
+FILE,
+JSON.stringify(
+data,
+null,
+2
 )
 );
 
-res.json(data);
+res.json({
+success:true
+});
+
+});
+
+/* duyuru düzenle */
+
+app.put(
+"/api/announcement/:id",
+(req,res)=>{
+
+const id =
+Number(req.params.id);
+
+let data =
+JSON.parse(
+fs.readFileSync(FILE)
+);
+
+data =
+data.map(a=>{
+
+if(a.id === id){
+
+return {
+...a,
+...req.body
+};
+
+}
+
+return a;
+
+});
+
+fs.writeFileSync(
+FILE,
+JSON.stringify(
+data,
+null,
+2
+)
+);
+
+res.json({
+success:true
+});
 
 });
 

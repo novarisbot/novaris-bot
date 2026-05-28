7754b9fae89b6d7943da737bc8d7171df17327f3
novarisbot <kerembeyy3500@gmail.com>
2026-05-28 12:09:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,55 @@
-const cursor = document.querySelector('.cursor');
-const blur = document.querySelector('.cursor-blur');
+const cursor =
+document.querySelector('.cursor');
 
-window.addEventListener('mousemove', (e) => {
-  cursor.style.left = e.clientX + 'px';
-  cursor.style.top = e.clientY + 'px';
+const blur =
+document.querySelector('.cursor-blur');
 
-  blur.style.left = e.clientX + 'px';
-  blur.style.top = e.clientY + 'px';
+window.addEventListener(
+'mousemove',
+(e)=>{
+
+cursor.style.left =
+e.clientX + 'px';
+
+cursor.style.top =
+e.clientY + 'px';
+
+blur.style.left =
+e.clientX + 'px';
+
+blur.style.top =
+e.clientY + 'px';
+
+});
+
+/* announcement */
+
+fetch("/data/announcement.json")
+.then(res=>res.json())
+.then(data=>{
+
+if(data.enabled){
+
+document
+.getElementById(
+"announcement-bar"
+)
+.innerHTML = `
+
+<div class="announcement">
+
+<div class="announcement-content">
+
+🔥 ${data.title}
+—
+${data.text}
+
+</div>
+
+</div>
+
+`;
+
+}
+
 });
